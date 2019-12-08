9ac1b656e332ef77cf54fd124a4462834bb0d8c7
andrewjunyoung <andrew.young16@imperial.ac.uk>
2019-12-08 12:22:28 +0000

    SYM-18: Added prelim documentation for file_writers.

--- symboard/file_writers.py
+++ symboard/file_writers.py
@@ -1,7 +1,8 @@
-'''@author: Andrew J. Young
-@description: A file writer, which given input will output this as formatted
-data to a specified output file.
-'''
+"""
+@author: Andrew J. Young
+@description: This file contains the definitions of classes which write to the
+local file system, and the methods they call in order to do so.
+"""
 
 # Imports from third party packages.
 from os.path import exists, splitext
@@ -18,38 +19,81 @@ from symboard.errors import (
     WriteException, FileExistsException, ContentsNoneException
 )
 from symboard.keylayouts.keylayouts import Keylayout
-from symboard.settings import VERSION
-
-
-DEFAULT_OUTPUT_PATH = './a.keylayout'
+from symboard.settings import VERSION, DEFAULT_OUTPUT_PATH
 
 
 class FileWriter:
-    def change_postfix(self, path: str, new_postfix: str) -> str:
-        # Assert postfix is non null and purely alphanumeric.
-        prefix, old_postfix = splitext(path)
+    """ The base class for functions which write to the local file system.
 
-        if old_postfix != new_postfix: # Incorrect or missing postfix.
-            return prefix + '.' + new_postfix
+    Any classes which inherit from this class should have a custom
+    implementation of «write».
+    """
+    def change_postfix(self, path: str, extension: str) -> str:
+        """ A function which returns a string of the given path which replaces
+        the extension with the given extension.
+
+        Args:
+            path (str): The path which we want to check the extension of, and
+                change if it mismatches with <extension>. This path does not
+                need to include an extension.
+            extension (str): The extension which we want the path to have.
+
+        Returns:
+            str: The full path, as provided, but with the extension specified by
+                <extension>. If the provided path had an extension already,
+                then this extenison is replaced. Otherwise, the provided
+                extension is appended to the path.
+        """
+
+        # Assert postfix is non null and purely alphanumeric.
+        prefix, old_extension = splitext(path)
+
+        if old_extension != extension: # Incorrect or missing postfix.
+            return prefix + '.' + extension
         else: # Correct postfix.
             return path
 
-    def write(self, object_, output_path: str = DEFAULT_OUTPUT_PATH):
+    def write(self, object_, output_path: str = DEFAULT_OUTPUT_PATH) -> None:
+        """ A generic implementation of writing to file, which should be
+        implemented by the children of this class.
+
+        Args:
+            object_ (object): The object we want to write data from.
+            output_path (str): The path of the file we want to write to.
+                Defaults to DEFAULT_OUTPUT_PATH.
+        """
         pass
 
 
 class KeylayoutFileWriter(FileWriter):
     def contents(self, keylayout: Keylayout) -> str:
-        # Defined by the children of this class.
+        """ A generic implementation of getting the contents of a keylayout,
+        which should be implemented by the children of this class.
+
+        Args:
+            keylayout (Keylayout): The keylayout we want to get the contents of.
+
+        Returns:
+            str: ''
+        """
         return ''
 
     def write(
         self, keylayout: Keylayout, output_path: str = DEFAULT_OUTPUT_PATH
     ) -> None:
-        ''' Given an output file path, creates a file in that file path.
-        Assumes the file is not already present. Throws an error if this
-        is not true.
-        '''
+        """ Given an output file path, tries to create a file in that path using
+        the contents of <keylayout>.
+
+        Args:
+            keylayout (Keylayout): The keylayout we want to write data from.
+            output_path (str): The path of the file we want to write to.
+                Defaults to DEFAULT_OUTPUT_PATH.
+
+        Raises:
+            FileExistsException: If <output_path> already exists.
+            WriteException: If any error occurs while trying to create the
+            contents of <keylayout>, or to write the contents to file.
+        """
 
         # Ensure the file has the «.keylayout» postfix.
         output_path = self.change_postfix(output_path, 'keylayout')
@@ -60,14 +104,27 @@ class KeylayoutFileWriter(FileWriter):
             raise FileExistsException()
 
         try:
+            contents = self.contents(keylayout)
             with open(output_path, 'w+') as file_:
-                file_.write(self.contents(keylayout))
+                file_.write(contents)
         except:
             raise WriteException()
 
 
 class KeylayoutXMLFileWriter(KeylayoutFileWriter):
     def contents(self, keylayout: Keylayout) -> str:
+        """
+        Args:
+            keylayout (Keylayout): The keylayout we want to get the contents of.
+
+        Returns:
+            str: The (formatted) contents of <keylayout>, in XML version 1.1, in
+            a format that can be read and installed by macOS.
+
+        Raises:
+            ContentsNoneException: If <keylayout> is None.
+        """
+
         if keylayout is None:
             raise ContentsNoneException()
 
@@ -91,25 +148,70 @@ class KeylayoutXMLFileWriter(KeylayoutFileWriter):
 
 
     def keyboard(self, keylayout: Keylayout) -> Element:
+        """
+        Args:
+            keylayout (Keylayout): The keylayout we want to create a root
+                element for.
+
+        Returns:
+            Element: An XML tag with the tag 'keyboard', and the attributes of
+                <keylayout>. This element can have children added to it later.
+        """
         return Element('keyboard', keylayout.keyboard_attributes())
 
     def version(self) -> str:
+        """
+        Returns:
+            str: A string specifying the version and encoding used by the class.
+        """
         return '<?xml version="1.1" encoding="UTF-8"?>'
 
     def comment(self, msg: str) -> str:
+        """
+        Returns:
+            str: A string representing an XML comment containing the provided
+            message.
+        """
         return '<!-- {} -->'.format(msg)
 
     def created(self) -> str:
+        # TODO: Synchronize with «updated»
+        """
+        Returns:
+            str: A string representing an XML comment containing the version of
+            symboard which is being used, and the date and time at which the
+            comment was created.
+        """
         return self.comment('Created by Symboard version {} at {}'.format(
             VERSION, datetime.utcnow() # TODO: ISO
         ))
 
     def updated(self) -> str:
+        """
+        Returns:
+            str: A string representing an XML comment containing the version of
+            symboard which is being used, and the date and time at which the
+            comment was created (as this is equal to its last update time).
+        """
+        # TODO: Synchronize with «created»
         return self.comment('Last updated by Symboard version {} at {}'.format(
             VERSION, datetime.utcnow() # TODO: ISO
         ))
 
     def layouts(self, keylayout: Keylayout, keyboard: Element) -> Element:
+        """
+        Args:
+            keylayout (Keylayout): The keylayout to create a «layouts» element
+                from.
+            keyboard (Element): The XML Element which is to be the parent of the
+            newly created «layouts» element.
+
+        Returns:
+            Element: An element which has been added as a child to <keyboard>,
+            containing the tag «layouts». It has children with the tag «layout»,
+            each containing attributes as specified by the attributes in
+            <keylayout.layouts>.
+        """
         layouts_elem = sub_element(keyboard, 'layouts')
 
         # Create children to the layouts_elem
@@ -119,6 +221,20 @@ class KeylayoutXMLFileWriter(KeylayoutFileWriter):
         return layouts_elem
 
     def modifier_map(self, keylayout: Keylayout, keyboard: Element) -> Element:
+        """
+        Args:
+            keylayout (Keylayout): The keylayout to create a «layouts» element
+                from.
+            keyboard (Element): The XML Element which is to be the parent of the
+            newly created «layouts» element.
+
+        Returns:
+            Element: An element which has been added as a child to <keyboard>,
+            containing the tag «modiferMap». It has children with the tag
+            «keyMapSelect», which themselves have children with the tag
+            «modifier». Each of these elements contains attributes as specified
+            by the dictionary in <keylayout.key_map_select>.
+        """
         modifier_map_elem = sub_element(
             keyboard,
             'modifierMap',
@@ -144,6 +260,20 @@ class KeylayoutXMLFileWriter(KeylayoutFileWriter):
         return modifier_map_elem
 
     def key_map_set(self, keylayout: Keylayout, keyboard: Element) -> Element:
+        """
+        Args:
+            keylayout (Keylayout): The keylayout to create a «layouts» element
+                from.
+            keyboard (Element): The XML Element which is to be the parent of the
+            newly created «layouts» element.
+
+        Returns:
+            Element: An element which has been added as a child to <keyboard>,
+            containing the tag «keyMapSet». It has children with the tag
+            «keyMap», which themselves have children with the tag «keys». Each
+            of these elements contains attributes as specified by the dictionary
+            in <keylayout.key_map>.
+        """
         key_map_set_elem = sub_element(keyboard, 'keyMapSet', {'id': 'ANSI'})
 
         # Create children to the key_map_set_elem
