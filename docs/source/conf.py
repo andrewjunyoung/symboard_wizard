cd108669c67ab993cc8b72bb7c82c884d29cc3ef
andrewjunyoung <andrew.young16@imperial.ac.uk>
2019-12-08 13:03:56 +0000

    SYM-18: Added sphinx extensions.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,8 +31,9 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
     'sphinx.ext.napoleon',
-    'sphinx.ext.autodoc', # TODO: There is one more that I've forgotten...
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -41,7 +42,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns: list = []
 
 
 # -- Options for HTML output -------------------------------------------------
