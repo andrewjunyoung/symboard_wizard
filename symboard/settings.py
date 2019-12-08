561dcbc138844ee4160acf6ae5aef70c6e605236
andrewjunyoung <andrew.young16@imperial.ac.uk>
2019-12-08 12:22:38 +0000

    SYM-18: Added prelim documentation for settings.py

--- symboard/settings.py
+++ symboard/settings.py
@@ -1,1 +1,17 @@
+"""
+@author: Andrew J. Young
+@description: A file containing settings used globally throughout Symboard. They
+have been placed in one file so that they're easy to configure.
+"""
+
+""" The current version of symboard being used, equivalent to a string
+<major_version>.<minor_version>
+"""
 VERSION = '0.1'
+
+""" If no output path is specified, the keylayout will default to being written
+to the path specified by DEFAULT_OUTPUT_PATH. This string can, but does not need
+to, end in «.keylayout». TODO: Check this last statement.
+"""
+DEFAULT_OUTPUT_PATH = './a.keylayout'
+
