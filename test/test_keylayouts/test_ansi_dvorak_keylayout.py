76afd912e99106dd07158a0b3f1cf78deabe6ce1
andrewjunyoung <andrew.young16@imperial.ac.uk>
2019-12-23 22:41:28 +0000

    SYM-14: Added ansi_dvorak_keylayout and the beginnings of tests for it. Integration test needs work. Will pull from develop to get code from there.

---
+++ test/test_keylayouts/test_ansi_dvorak_keylayout.py
@@ -0,0 +1,104 @@
+'''
+@author: Andrew J. Young
+@description: Acceptance tests for the ansi_dvorak_keyboard
+'''
+
+# Imports from third party packages
+from unittest import TestCase
+from unittest import main as unittest_main
+from unittest.mock import patch, Mock, MagicMock
+from os import remove
+
+# Package internal imports
+from symboard.keylayouts.ansi_dvorak_keylayout import AnsiDvorakKeylayout
+from symboard.file_writers import KeylayoutXMLFileWriter
+from test.test_keylayouts.test_keylayouts import TestKeylayout
+
+
+file_writers_path = 'symboard.file_writers'
+
+
+class TestAnsiDvorakKeylayout(TestKeylayout):
+    def setUp(self):
+        self.OUTPUT_PATH = 'actual.keylayout'
+        self.EXPECTED_ANSI_KEYLAYOUT_PATH = 'test/res/' \
+            'expected_ansi_dvorak_keylayout.keylayout'
+
+        self.GROUP = 1
+        self.ID = 2
+        self.MAXOUT = 3
+        self.NAME = '4'
+        self.DEFAULT_INDEX = 5
+
+        self.keylayout = AnsiDvorakKeylayout(
+            self.GROUP,
+            self.ID,
+            self.MAXOUT,
+            name = self.NAME,
+            default_index = self.DEFAULT_INDEX
+        )
+
+    def test_keylayout_str(self):
+        expected = 'AnsiDvorakKeylayout({}, (id: {}))'.format(self.NAME, self.ID)
+        actual = str(self.keylayout)
+
+        self.assertEqual(expected, actual)
+
+
+class TestAnsiDvorakKeyboardIntegration(TestCase):
+    def setUp(self):
+        self.EXPECTED_ANSI_KEYLAYOUT_PATH = 'test/res/' \
+            'expected_ansi_dvorak_keylayout.keylayout'
+        self.OUTPUT_PATH = 'actual'
+
+        self.GROUP = 126
+        self.ID = -19341
+        self.MAXOUT = 1
+        self.DEFAULT_INDEX = 6
+
+        self.ansi_dvorak_keylayout = AnsiDvorakKeylayout(
+            self.GROUP,
+            self.ID,
+            self.MAXOUT,
+            default_index = self.DEFAULT_INDEX
+        )
+        self.keylayout_xml_file_writer = KeylayoutXMLFileWriter()
+
+        self.maxDiff = None
+
+        self.mock = Mock()
+
+    @patch(file_writers_path + '.datetime')
+    def test_output_is_as_expected(self, datetime):
+        '''
+        !!! WARNING: This function *will* write to disk !!!
+        '''
+        # Setup.
+        mock_datetime = self.mock
+        datetime.now = MagicMock(return_value=mock_datetime)
+        mock_datetime.strftime.return_value = '2019-12-07 21:54:51 (UTC)'
+
+        # Execution.
+        self.keylayout_xml_file_writer.write(
+            self.ansi_dvorak_keylayout, self.OUTPUT_PATH
+        )
+
+        try:
+            with open(self.OUTPUT_PATH + '.keylayout', 'r') as file_:
+                actual = file_.read()
+            remove(self.OUTPUT_PATH + '.keylayout')
+        except:
+            remove(self.OUTPUT_PATH + '.keylayout')
+            self.fail() # We should never get here.
+
+
+        with open(self.EXPECTED_ANSI_KEYLAYOUT_PATH, 'r') as file_:
+            expected = file_.read()
+
+        # Assertion.
+        self.assertEqual(expected, actual)
+
+
+if __name__ == '__main__':
+    unittest_main()
+
