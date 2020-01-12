77f2a588a74f6d2347528b7b6273428bc460139c
andrewjunyoung <andrew.young16@imperial.ac.uk>
2020-01-12 15:04:30 +0000

    Updated setup file with more extensible name; version format.

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
 import setuptools
+from symboard import __name__, __version__
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name = 'symboard',
-    version = '0.2.0',
-    author = 'Andrew J. Young',
-    author_email = 'andrewjunyoung1@gmail.com',
-    description = 'A text-based keyboard layout creator. Change or add keys ' \
+    name=__name__,
+    version=__version__,
+    author='Andrew J. Young',
+    author_email='andrewjunyoung1@gmail.com',
+    description='A text-based keyboard layout creator. Change or add keys ' \
         'to your keyboard with ease.',
-    long_description = long_description,
-    long_description_content_type = 'text/markdown',
-    url = 'https://github.com/andrewjunyoung/symboard',
-    packages = setuptools.find_packages(),
-    classifiers = [
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/andrewjunyoung/symboard',
+    packages=setuptools.find_packages(),
+    classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires = '>=3.6',
+    python_requires='>=3.6',
 )
