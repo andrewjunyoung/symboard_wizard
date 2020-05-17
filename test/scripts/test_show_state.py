bb1b637fe8bca6eedbc83391ad1b6d85ac2d00eb
ajy <andrew@sierracompute.com>
2020-05-17 14:46:20 +0100

    SYM-56: Hacked the tests into working because MagicMock already has a «name» property, making it hard to mock attributes with this name.

--- test/scripts/test_show_state.py
+++ test/scripts/test_show_state.py
@@ -1,48 +1,68 @@
+# From third party packages.
+from io import StringIO
+from tabulate import tabulate
 from unittest import TestCase
-from unittest.mock import MagicMock, mock_open, patch
 from unittest import main as unittest_main
+from unittest.mock import MagicMock, mock_open, patch, PropertyMock
+
+# From the local package.
 from scripts.show_states import main, format_outputs
 
 
-show_states_path = 'scripts/show_states.py'
+show_states_path = 'scripts.show_states'
 
 
 class TestShowStates(TestCase):
     def setUp(self):
-        self.outputs = ['a','b','c','d','e','f','g']
-        attrs = {'action_to_output_map.values.return_value': self.outputs}
+        self.output_list = ['a','b','c','d','e','f','g']
+        self.outputs = ''.join(self.output_list)
+        attrs = {'action_to_output_map.values.return_value': self.output_list}
 
         self.state_name = 'state_name'
         self.state_terminator = '.'
 
+        # MagicMock has issues mocking the property «name» because it uses this
+        # keyword argument in its own initialization function. The below is a
+        # workaround.
+        name = PropertyMock(return_value=self.state_name)
         self.state = MagicMock(
-            name = self.state_name,
             terminator = self.state_terminator,
             **attrs
         )
+        type(self.state).name = name
 
     def test_format_outputs(self):
-        expected = ''.join(self.outputs)
         actual = format_outputs(self.state)
+        self.assertEqual(self.outputs, actual)
 
-        self.assertEqual(expected, actual)
-
+    @patch(show_states_path + '.tabulate')
     @patch(show_states_path + '.load_yaml')
-    def test_main(self, load_yaml):
+    def test_main(self, load_yaml, tabulate):
         # Setup.
+        data = [
+            [self.state_name, self.state_terminator, self.outputs]
+        ]
         expected_headers = ['Name', 'Terminator', 'Outputs']
-        expected_output = tabulate(
-            data, headers=expected_headers, tablefmt='orgtbl'
-        )
+
+        output_string = 'output'
+        expected_output = output_string + '\n'
 
         load_yaml.return_value = {self.state_name: self.state}
+        tabulate.return_value = output_string
 
-        # Execution.
-        main()
+        with patch('sys.stdout', new = StringIO()) as fake_out:
+            # Execution.
+            main()
 
-        # Assertion.
-        self.assertEqual(expected_output, actual_output)
+            # Assertion.
+            self.assertEqual(expected_output, fake_out.getvalue())
+            tabulate.assert_called_once_with(
+                data,
+                headers=expected_headers,
+                tablefmt='orgtbl'
+            )
 
 
 if __name__ == '__main__':
     unittest_main()
+
